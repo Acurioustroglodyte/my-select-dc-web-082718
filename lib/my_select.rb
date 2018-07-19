2c7f7a8d19f5a5d5b0df2e6a8d5faf29e356ccdf
Trevor Jameson <tjameson15@gmail.com>
2018-07-19 11:58:31 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def my_select(collection)
  if collection.size == 0
    return false
@@ -6,6 +8,7 @@ def my_select(collection)
    counter = 0
    while counter < collection.length
      arr_clone[counter] = yield collection[counter]
+     puts arr_clone[counter]
      counter += 1
    end
     return arr_clone
